18d2c0795d7ba9b51f5a8a030b581eec0626f177
Tokio-svg <tokkipt935@gmail.com>
2022-09-01 22:51:54 +0900

    config/database.php修正

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_URI', null),
-            'database' => env('DB_DATABASE'),
+            'database' => env('DB_DATABASE', 'forge'),
             'host' => env('DB_HOST', '127.0.0.1')
         ],
 
