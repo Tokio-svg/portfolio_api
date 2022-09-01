929dd87e6efc2cbbd5c075bfb8763ff63829bf2c
Tokio-svg <tokkipt935@gmail.com>
2022-09-01 20:59:15 +0900

    mongoDBのURI記述を削除

--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('DB_URI', 'mongodb+srv://tokioman:7mRtB5m6m1ayOaZy@cluster0.rfqjoxk.mongodb.net/?retryWrites=true&w=majority'),
+            'dsn' => env('DB_URI', null),
             'database' => env('DB_DATABASE'),
         ],
 
